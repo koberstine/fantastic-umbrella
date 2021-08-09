47dbb37a245b45f2d0ad54de369f4ba65f248c17
Hammurabi <koberstine@hotmail.com>
2021-08-09 17:03:57 -0400

    added product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,7 +42,7 @@ router.get('/:id', (req, res) => {
     })
       .then(dbCategoryData => {
         if (!dbCategoryData) {
-          res.status(404).json({ message: 'No post found with this id' });
+          res.status(404).json({ message: 'No category found with this id' });
           return;
         }
         res.json(dbCategoryData);
