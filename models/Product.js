9b51e6206fd0a6a88fffff1862f7f6de1cf39f78
Hammurabi <koberstine@hotmail.com>
2021-08-09 20:19:27 -0400

    debugging

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-const { defaultValueSchemable } = require('sequelize/types/lib/utils');
+// const { defaultValueSchemable } = require('sequelize/types/lib/utils');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
